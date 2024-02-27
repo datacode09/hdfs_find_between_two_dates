27406cf5947c2edfe9a3aeb588c76845bc49a058
Abul Fahad <117757435+datacode09@users.noreply.github.com>
2024-02-27 13:52:32 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
 hdfs dfs -ls /path/to/your/folder | awk 'BEGIN{start=strtotime("20240201"); end=strtotime("20240210")} {current=strtotime($6$7)} current>=start && current<=end'
+hdfs dfs -ls /path/to/your/folder | awk -v start="YYYY-MM-DD" -v end="YYYY-MM-DD" '$6 >= start && $6 <= end'
